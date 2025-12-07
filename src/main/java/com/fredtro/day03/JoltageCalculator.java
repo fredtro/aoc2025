5833335c382aa5dd40b16bfca01bda1c19661e0e
fredtro <fredtrostorf@hotmail.com>
2025-12-07 12:44:05 +0100

    add algorithm for day 3b

--- src/main/java/com/fredtro/day03/JoltageCalculator.java
+++ src/main/java/com/fredtro/day03/JoltageCalculator.java
@@ -2,8 +2,7 @@ package com.fredtro.day03;
 
 import com.fredtro.util.FileReader;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class JoltageCalculator {
@@ -45,11 +44,72 @@ public class JoltageCalculator {
             .sum();
     }
 
+    public static long getResultB() {
+        return getLines().stream()
+            .map(String::toCharArray)
+            .map(chars -> computeResultForLine(chars, 12))
+            .mapToLong(Long::longValue)
+            .sum();
+    }
+
+    private static long computeResultForLine(char[] chars, int totalSpots) {
+
+        // Create elements ahead of time
+        List<Element> elements = new ArrayList<>(chars.length);
+        for (int i = 0; i < chars.length; i++) {
+            elements.add(new Element(i, chars[i]));
+        }
+
+        Set<Element> result = new LinkedHashSet<>(totalSpots);
+        Element max = getMax(elements);
+
+        while (result.size() < totalSpots && max != null) {
+
+            int remainingSlots = totalSpots - result.size() - 1;
+            int itemsRight = chars.length - max.index() - 1;
+
+            if (itemsRight >= remainingSlots) {
+                // we can safely pick this max
+                result.add(max);
+
+                int idx = max.index();
+                // only keep elements strictly to the right
+                elements = elements.stream()
+                    .filter(e -> e.index() > idx)
+                    .toList();
+
+                max = getMax(elements);
+
+            } else {
+                // we must search left side instead
+                int idx = max.index();
+                List<Element> leftSide = elements.stream()
+                    .filter(e -> e.index() < idx && !result.contains(e))
+                    .toList();
+
+                max = getMax(leftSide);
+            }
+        }
+
+        // Build number
+        long value = 0;
+        for (Element e : result) {
+            value = value * 10 + (e.value() - '0');
+        }
+
+        return value;
+    }
+
+    private static Element getMax(List<Element> elements) {
+        return elements.stream()
+            .max(Comparator.comparingInt(Element::value))
+            .orElse(null);
+    }
+
 
     private static List<String> getLines() {
         return FileReader.parse("/day03/input.txt");
     }
-
 
     record Element(int index, char value) {
         @Override
